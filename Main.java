722274083cda5d9e472f8eda78fad99073177f6e
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-04-29 22:00:31 +0200

    Add linter

---
+++ Main.java
@@ -0,0 +1,13 @@
+/**
+ * This is the Main class.
+ */
+public class Main {
+  /**
+   * This is the main method.
+   *
+   * @param args the command-line arguments
+   */
+  public static void main(String[] args) {
+    return;
+  }
+}
