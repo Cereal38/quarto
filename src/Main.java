32e3f1535356f905cac0271d2d35a731d5b25e44
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-04-30 12:36:20 +0200

    Add file structure

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,9 @@ package src;
  * This is the Main class.
  */
 public class Main {
-  /**
-   * This is the main method.
-   *
-   * @param args the command-line arguments
-   */
+
   public static void main(String[] args) {
+
     return;
   }
 }
