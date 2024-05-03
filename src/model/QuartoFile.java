109794417e221606ed7c38bf0e4551e9050f09ba
Orihsoth <71321233+Orihsoth@users.noreply.github.com>
2024-05-03 15:50:55 +0200

    Deplacement des méthode de victoire + quelques corrections

--- src/model/QuartoFile.java
+++ src/model/QuartoFile.java
@@ -17,15 +17,11 @@ public class QuartoFile {
     }
 
     public boolean canRedo() {
-        if (save.next != null)
-            return true;
-        return false;
+        return save.next != null;
     }
 
     public boolean canUndo() {
-        if (save.precedent != null)
-            return true;
-        return false;
+        return save.precedent != null;
     }
 
     public void saveFile(String fileName) throws IOException {
@@ -34,13 +30,13 @@ public class QuartoFile {
             FileWriter fileWriter = new FileWriter(fileName);
             PrintWriter printWriter = new PrintWriter(fileWriter);
             QuartoHistory head_cp = head;
-            while (head_cp != null) { //while we go through all the history we have
+            while (head_cp != null) { //while we go through all the history
                 if (head_cp.equals(save)) {//If we are on the current move, we'll write a special caractere to know which move we'll return.
-                    if (head_cp.state == 0) {// If we have placed a pawn
-                        //we write all the information of the pawn
+                    if (head_cp.state == 0) {// If we placed a pawn
+                        //we write the information about the pawn
                         printWriter.print(head_cp.indexPawn + " *\n");
-                    } else if (head_cp.state == 1) {//If we choosed a pawn to give to the next player
-                        //we write the position of the pawn we placed.
+                    } else if (head_cp.state == 1) {//If we chose a pawn to give to the next player
+                        //we write the position of the pawn we chose.
                         printWriter.print(head_cp.line + " " + head_cp.column + " *\n");
                     }
                 } else {
@@ -60,21 +56,21 @@ public class QuartoFile {
 
     public void chargeFile(String fileName) {
         InputStream in = null;
-		String[] line;
-		head = new QuartoHistory();
-		QuartoHistory temp = head;
-		try {
-      		in = new FileInputStream(fileName);
-			Scanner s = new Scanner(in);
-			while (s.hasNextLine()) {
-				line = s.nextLine().split(" ");
-				if (line.length == 1) { //state == 0
-					temp.next = new QuartoHistory(Integer.parseInt(line[0]), temp);
-					temp.next.precedent = temp;
-					temp = temp.next;
-				} else if (line.length == 2) {//(state == 1) or (state == 0 and actual state)
-					if (line[1].equals("*")) {
-					    temp.next = new QuartoHistory(Integer.parseInt(line[0]), temp);
+        String[] line;
+        head = new QuartoHistory();
+        QuartoHistory temp = head;
+        try {
+            in = new FileInputStream(fileName);
+            Scanner s = new Scanner(in);
+            while (s.hasNextLine()) {
+                line = s.nextLine().split(" ");
+                if (line.length == 1) { //state == 0
+                    temp.next = new QuartoHistory(Integer.parseInt(line[0]), temp);
+                    temp.next.precedent = temp;
+                    temp = temp.next;
+                } else if (line.length == 2) {//(state == 1) or (state == 0 and actual state)
+                    if (line[1].equals("*")) {
+                        temp.next = new QuartoHistory(Integer.parseInt(line[0]), temp);
                         temp.next.precedent = temp;
                         temp = temp.next;
                         save = temp;
@@ -88,11 +84,11 @@ public class QuartoFile {
                     temp.next.precedent = temp;
                     temp = temp.next;
                     save = temp;
-				}
-			}
-			s.close();
-		} catch (FileNotFoundException e) {
-			System.err.println("impossible de trouver le fichier " + fileName);
-		}
+                }
+            }
+            s.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("impossible de trouver le fichier " + fileName);
+        }
     }
 }
--- src/model/QuartoModel.java
+++ src/model/QuartoModel.java
@@ -1,11 +1,20 @@
 package src.model;
 
+/*
+ * TODO
+ * tester l'historique
+ * faire une méthode pour renvoyer la liste des booléens de la méthode winsituations.
+ * get et set pour les variables.
+ * crée class QuartoWin
+ */
+
 public class QuartoModel {
     private QuartoPawn[][] table;
     private int player;//1 for Player 1 and 2 for Player 2
     private QuartoPawn[] pawnAvailable;
     private QuartoPawn selectedPawn;
     QuartoFile histo;
+    QuartoWin win;
     
     private void newTable() {
         table = new QuartoPawn[4][4];//table filled with null
@@ -19,26 +28,14 @@ public class QuartoModel {
     public QuartoModel() {
         newTable();
         histo = new QuartoFile();
-    }
-
-    private void removePawn(int pawnRemoved) {
-        QuartoPawn[] newList = new QuartoPawn[pawnAvailable.length - 1];
-        int j = 0;
-        for(int i = 0; i < newList.length; i++){
-            if(j == pawnRemoved){
-                j++;
-            }
-            newList[i] = pawnAvailable[j];
-            j++;
-        }
-        pawnAvailable = newList;
+        win = new QuartoWin();
     }
 
     public void redo() {
         if (histo.canRedo()) {
             if (histo.save.next.state == 0) {//choice of pawn
                 selectedPawn = pawnAvailable[histo.save.next.indexPawn];
-                removePawn(histo.save.next.indexPawn);
+                pawnAvailable[histo.save.next.indexPawn] = null;
                 //switchplayer
             } else if (histo.save.next.state == 1) { //choice of place
                 table[histo.save.next.line][histo.save.next.column] = selectedPawn;
@@ -47,50 +44,36 @@ public class QuartoModel {
         }
     }
 
-    //We add a pawn we removed from the game to can use it again.
-    private void addPawnAvailable(int indexPawn) {
-        //we grow the list of pawn available;
-        QuartoPawn[] newPawnAvailable = new QuartoPawn[pawnAvailable.length + 1];
-        int count_old = 0;
-        for (int count_new = 0; count_new < pawnAvailable.length + 1; count_new++) {
-            //if the index of the pawn we want to add is equal to the counter, then we add it a this index.
-            if (count_new == indexPawn) {
-                newPawnAvailable[count_new] = selectedPawn;
-                count_new++;
-            }
-            newPawnAvailable[count_new] = pawnAvailable[count_old];
-            count_old++;
-        }
-        pawnAvailable = newPawnAvailable;
-    }
-
     public void undo() {
         if (histo.canUndo()) {
             if (histo.save.precedent.state == 0) {//we remove a pawn we placed
+                //get last pawn
                 table[histo.save.line][histo.save.column] = null;
                 //switchplayer
             } else if (histo.save.precedent.state == 1) {//we add the pawn choosen to the list of pawn available.
-                addPawnAvailable(histo.save.indexPawn);
+                pawnAvailable[histo.save.indexPawn] = getSelectedPawn();
+                setSelectedPawn(null);
             }
             histo.save = histo.save.precedent;
         }
     }
 
-    public boolean isTableEmpty(int line, int column) {
-        return table[line][column] == null;
-    }
-
     public void selectPawn(int pawnRemoved) {
-        selectedPawn = pawnAvailable[pawnRemoved];
+        setSelectedPawn(pawnAvailable[pawnRemoved]);
         //Add a new history because we chose what pawn the next player will play.
         histo.save.next = new QuartoHistory(pawnRemoved, histo.save);
         histo.save.next.precedent = histo.save;
         histo.save = histo.save.next;
-        removePawn(pawnRemoved);
+
+        pawnAvailable[pawnRemoved] = null;
     }
 
     public boolean isPawnListEmpty() {
-        return pawnAvailable.length == 0;
+        for (int i = 0; i < 16; i++) {
+            if (pawnAvailable[i] != null)
+                return false;
+        }
+        return true;
     }
     
     public void switchPlayer() {
@@ -98,8 +81,9 @@ public class QuartoModel {
     }
 
     public void playShot(int line, int column) {
-        if (isTableEmpty(line, column)) {
+        if (win.isTableEmpty(table, line, column)) {
             table[line][column] = selectedPawn;
+            winSituation(line, column);
             histo.save.next = new QuartoHistory(line, column, histo.save);
             histo.save.next.precedent = histo.save;
             histo.save = histo.save.next;
@@ -107,98 +91,9 @@ public class QuartoModel {
     }
 
     public boolean winSituation(int line, int column) {
-        return (winSituationLine(line) || winSituationColumn(column)
-                || winSituationDiagonal(line, column));
-    }
 
-    private boolean winSituationLine(int line) {
-        if (isTableEmpty(line, 0))
-                return false;
-        boolean round = table[line][0].isRound();
-        boolean rTrue = true;
-        boolean white = table[line][0].isWhite();
-        boolean wTrue = true;
-        boolean little = table[line][0].isLittle();
-        boolean lTrue = true;
-        boolean hollow = table[line][0].isWhite();
-        boolean hTrue = true;
-        for (int i = 1; i < 4; i++) {
-            if (isTableEmpty(line, i))
-                return false;
-            if (rTrue && table[line][i].isRound() != round)
-                rTrue = false;
-            if (wTrue && table[line][i].isWhite() != white)
-                wTrue = false;
-            if (lTrue && table[line][i].isLittle() != little)
-                lTrue = false;
-            if (hTrue && table[line][i].isHollow() != hollow)
-                hTrue = false;
-        }
-        return rTrue || wTrue || lTrue || hTrue;
-    }
-    
-    private boolean winSituationColumn(int column) {
-        if (isTableEmpty(0, column))
-            return false;
-        boolean round = table[0][column].isRound();
-        boolean rTrue = true;
-        boolean white = table[0][column].isWhite();
-        boolean wTrue = true;
-        boolean little = table[0][column].isLittle();
-        boolean lTrue = true;
-        boolean hollow = table[0][column].isHollow();
-        boolean hTrue = true;
-        for (int i = 1; i < 4; i++) {
-            if (isTableEmpty(i, column))
-                return false;
-            if (rTrue && table[i][column].isRound() != round)
-                rTrue = false;
-            if (wTrue && table[i][column].isWhite() != white)
-                wTrue = false;
-            if (lTrue && table[i][column].isLittle() != little)
-                lTrue = false;
-            if (hTrue && table[i][column].isHollow() != hollow)
-                hTrue = false;
-        }
-        return rTrue || wTrue || lTrue || hTrue;
-    }
-    
-    private boolean winSituationDiagonal(int line, int column) {
-        int addColumn;
-        if (line == column) {
-            addColumn = 1;
-            column = 0;
-        } else if ((line + column) == 3) {
-            addColumn = -1;
-            column = 3;
-        } else {
-            return false;
-        }
-        if (isTableEmpty(0, column))
-            return false;
-        boolean round = table[0][column].isRound();
-        boolean rTrue = true;
-        boolean white = table[0][column].isWhite();
-        boolean wTrue = true;
-        boolean little = table[0][column].isLittle();
-        boolean lTrue = true;
-        boolean hollow = table[0][column].isHollow();
-        boolean hTrue = true;
-
-        for (int i = 0; i < 4; i++) {
-            if (isTableEmpty(i, column))
-                return false;
-            if (rTrue && table[i][column].isRound() != round)
-                rTrue = false;
-            if (wTrue && table[i][column].isWhite() != white)
-                wTrue = false;
-            if (lTrue && table[i][column].isLittle() != little)
-                lTrue = false;
-            if (hTrue && table[i][column].isHollow() != hollow)
-                hTrue = false;
-            column = column + addColumn;
-        }
-        return rTrue || wTrue || lTrue || hTrue;
+        return (win.winSituationLine(table, line) || win.winSituationColumn(table, column)
+                || win.winSituationDiagonal(table, line, column));
     }
     
     public void chargeGame(String fileName) {
@@ -207,14 +102,13 @@ public class QuartoModel {
         while(!copy.equals(histo.save)) {
             if (copy.state == 0) {
                 selectedPawn = pawnAvailable[copy.indexPawn];
-                removePawn(copy.indexPawn);
+                pawnAvailable[copy.indexPawn] = null;
             } else if (copy.state == 1) {
                 table[copy.line][copy.column] = selectedPawn;
             }
             copy = copy.next;
         }
     }
-
 
     public QuartoPawn getSelectedPawn() {
         return selectedPawn;
