4c425a2de32bde015b9a868e2054e202c8b57857
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-05-04 07:09:45 +0200

    Move navbar to it's own file

--- src/views/MainMenu.java
+++ src/views/MainMenu.java
@@ -1,69 +1,23 @@
 package src.views;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
-import src.views.utils.ImageUtils;
 
 public class MainMenu extends JPanel {
   private JButton btnPlayerVsPlayer;
   private JButton btnPlayerVsAI;
   private JButton btnLoad;
-  private JButton musicButton, langButton, modeButton, exitButton, bookButton;
 
   public MainMenu(ActionListener actionListener) {
     setLayout(new BorderLayout());
 
-    // Chargement des icônes
-    ImageIcon musicOnImg = ImageUtils.loadImage("music-on.png", 30, 30);
-    ImageIcon musicOffImg = ImageUtils.loadImage("music-off.png", 30, 30);
-    ImageIcon darkImg = ImageUtils.loadImage("dark.png", 30, 30);
-    ImageIcon lightImg = ImageUtils.loadImage("light.png", 30, 30);
-    ImageIcon exitImg = ImageUtils.loadImage("exit.png", 30, 30);
-    ImageIcon frImg = ImageUtils.loadImage("fr.png", 30, 30);
-    ImageIcon enImg = ImageUtils.loadImage("en.png", 30, 30);
-    ImageIcon bookImg = ImageUtils.loadImage("book.png", 30, 30);
-
-    // Création des boutons
-    musicButton = ImageUtils.createButtonFromImage(musicOnImg);
-    modeButton = ImageUtils.createButtonFromImage(darkImg);
-    langButton = ImageUtils.createButtonFromImage(enImg);
-    exitButton = ImageUtils.createButtonFromImage(exitImg);
-    bookButton = ImageUtils.createButtonFromImage(bookImg);
-
-    // Définir des action commands pour chaque bouton
-    musicButton.setActionCommand("Music");
-    modeButton.setActionCommand("DarkMode"); // Correspond à ce que "actionPerformed" attend
-    langButton.setActionCommand("Language");
-    exitButton.setActionCommand("Quit");
-    bookButton.setActionCommand("Manual");
-
-    // Navbar avec des boutons alignés différemment
-    JPanel navbar = new JPanel(new BorderLayout());
-
-    // Panneau pour les boutons alignés à gauche
-    JPanel leftPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-    leftPanel.add(musicButton);
-    leftPanel.add(modeButton);
-    leftPanel.add(langButton);
-
-    // Panneau pour les boutons alignés à droite
-    JPanel rightPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-    rightPanel.add(bookButton);
-    rightPanel.add(exitButton);
-
-    // Ajout des sous-panneaux à la navbar
-    navbar.add(leftPanel, BorderLayout.WEST);
-    navbar.add(rightPanel, BorderLayout.EAST);
-
+    JPanel navbar = new TopBarMainMenu();
     add(navbar, BorderLayout.NORTH);
 
     // Button of the menu
@@ -103,18 +57,18 @@ public class MainMenu extends JPanel {
     btnPlayerVsPlayer.addActionListener(actionListener);
     btnPlayerVsAI.addActionListener(actionListener);
     btnLoad.addActionListener(actionListener);
-    exitButton.addActionListener(actionListener);
-    musicButton.addActionListener(actionListener);
-    langButton.addActionListener(actionListener);
-    modeButton.addActionListener(actionListener); // Ajout de l'ActionListener
-    bookButton.addActionListener(actionListener);
+    // exitButton.addActionListener(actionListener);
+    // musicButton.addActionListener(actionListener);
+    // langButton.addActionListener(actionListener);
+    // modeButton.addActionListener(actionListener); // Ajout de l'ActionListener
+    // bookButton.addActionListener(actionListener);
   }
 
-  public void addNavbarActionListener(ActionListener actionListener) {
-    musicButton.addActionListener(actionListener);
-    modeButton.addActionListener(actionListener);
-    langButton.addActionListener(actionListener);
-    bookButton.addActionListener(actionListener);
-    exitButton.addActionListener(actionListener);
-  }
+  // public void addNavbarActionListener(ActionListener actionListener) {
+  // musicButton.addActionListener(actionListener);
+  // modeButton.addActionListener(actionListener);
+  // langButton.addActionListener(actionListener);
+  // bookButton.addActionListener(actionListener);
+  // exitButton.addActionListener(actionListener);
+  // }
 }
---
+++ src/views/TopBarMainMenu.java
@@ -0,0 +1,58 @@
+package src.views;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import src.views.utils.ImageUtils;
+
+public class TopBarMainMenu extends JPanel {
+
+  private JButton musicButton, langButton, modeButton, exitButton, bookButton;
+
+  public TopBarMainMenu() {
+    // Load icons
+    ImageIcon musicOnImg = ImageUtils.loadImage("music-on.png", 30, 30);
+    ImageIcon musicOffImg = ImageUtils.loadImage("music-off.png", 30, 30);
+    ImageIcon darkImg = ImageUtils.loadImage("dark.png", 30, 30);
+    ImageIcon lightImg = ImageUtils.loadImage("light.png", 30, 30);
+    ImageIcon exitImg = ImageUtils.loadImage("exit.png", 30, 30);
+    ImageIcon frImg = ImageUtils.loadImage("fr.png", 30, 30);
+    ImageIcon enImg = ImageUtils.loadImage("en.png", 30, 30);
+    ImageIcon bookImg = ImageUtils.loadImage("book.png", 30, 30);
+
+    // Create buttons
+    musicButton = ImageUtils.createButtonFromImage(musicOnImg);
+    modeButton = ImageUtils.createButtonFromImage(darkImg);
+    langButton = ImageUtils.createButtonFromImage(enImg);
+    exitButton = ImageUtils.createButtonFromImage(exitImg);
+    bookButton = ImageUtils.createButtonFromImage(bookImg);
+
+    // Add actions to buttons
+    musicButton.setActionCommand("Music");
+    modeButton.setActionCommand("DarkMode");
+    langButton.setActionCommand("Language");
+    exitButton.setActionCommand("Quit");
+    bookButton.setActionCommand("Manual");
+
+    // Buttons aligned on the left
+    JPanel leftPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 12, 10));
+    leftPanel.add(musicButton);
+    leftPanel.add(modeButton);
+    leftPanel.add(langButton);
+
+    // Buttons aligned on the right
+    JPanel rightPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 12, 10));
+    rightPanel.add(bookButton);
+    rightPanel.add(exitButton);
+
+    setLayout(new BorderLayout());
+
+    add(leftPanel, BorderLayout.WEST);
+    add(rightPanel, BorderLayout.EAST);
+
+  }
+}
