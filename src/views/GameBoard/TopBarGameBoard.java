9c943c8258299c29fd79ad682307d143989113ba
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-05-05 12:07:47 +0200

    TopBarGameBoard is now finished

--- src/views/GameBoard/TopBarGameBoard.java
+++ src/views/GameBoard/TopBarGameBoard.java
@@ -1,9 +1,12 @@
 package src.views.GameBoard;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import src.views.components.TranslatedButton;
+import src.views.components.TranslatedLabel;
 import src.views.utils.EventsHandler;
 
 public class TopBarGameBoard extends JPanel {
@@ -11,6 +14,8 @@ public class TopBarGameBoard extends JPanel {
   private TranslatedButton btnUndo = new TranslatedButton("undo");
   private TranslatedButton btnRedo = new TranslatedButton("redo");
   private TranslatedButton btnPause = new TranslatedButton("pause");
+  private TranslatedLabel lblTurnOf = new TranslatedLabel("turn-of-player");
+  private JLabel lblPlayer = new JLabel("Player 1");
 
   public TopBarGameBoard() {
     setLayout(new BorderLayout());
@@ -32,6 +37,14 @@ public class TopBarGameBoard extends JPanel {
     add(rightPanel, BorderLayout.EAST);
     rightPanel.setLayout(new FlowLayout(FlowLayout.RIGHT, 12, 10));
     rightPanel.add(btnPause);
+
+    // Center panel with the current state of the game
+    JPanel centerPanel = new JPanel();
+    add(centerPanel, BorderLayout.CENTER);
+    centerPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 8, 15));
+    lblPlayer.setForeground(Color.BLUE);
+    centerPanel.add(lblTurnOf);
+    centerPanel.add(lblPlayer);
   }
 
 }
