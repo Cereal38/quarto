e161701d62bc06262345eee9b40d64de98380c5c
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-05-05 11:58:11 +0200

    Move the topbar of the gameboard to a new file

--- src/views/GameBoard/GameBoard.java
+++ src/views/GameBoard/GameBoard.java
@@ -1,34 +1,17 @@
 package src.views.GameBoard;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
-import src.views.components.TranslatedButton;
-import src.views.utils.EventsHandler;
 
 public class GameBoard extends JPanel {
-  private TranslatedButton btnUndo = new TranslatedButton("undo");
-  private TranslatedButton btnRedo = new TranslatedButton("redo");
-  private TranslatedButton btnPause = new TranslatedButton("pause");
 
   public GameBoard() {
     setLayout(new BorderLayout());
 
-    // Navbar with undo, redo and pause buttons
-    JPanel navbar = new JPanel(new FlowLayout(FlowLayout.LEFT));
-
-    // Add action listeners to the buttons
-    btnPause.addActionListener(e -> {
-      EventsHandler.showDialog(new PauseDialogContent());
-    });
-
-    navbar.add(btnUndo);
-    navbar.add(btnRedo);
-    navbar.add(btnPause);
-
-    add(navbar, BorderLayout.NORTH);
+    // Add the topbar
+    add(new TopBarGameBoard(), BorderLayout.NORTH);
 
     JPanel board = new JPanel(new GridLayout(4, 4));
     for (int i = 0; i < 16; i++) {
--- src/views/GameBoard/TopBarGameBoard.java
+++ src/views/GameBoard/TopBarGameBoard.java
@@ -1,12 +1,37 @@
 package src.views.GameBoard;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import javax.swing.JPanel;
+import src.views.components.TranslatedButton;
+import src.views.utils.EventsHandler;
 
 public class TopBarGameBoard extends JPanel {
 
+  private TranslatedButton btnUndo = new TranslatedButton("undo");
+  private TranslatedButton btnRedo = new TranslatedButton("redo");
+  private TranslatedButton btnPause = new TranslatedButton("pause");
+
   public TopBarGameBoard() {
     setLayout(new BorderLayout());
+
+    // Add action listeners to the buttons
+    btnPause.addActionListener(e -> {
+      EventsHandler.showDialog(new PauseDialogContent());
+    });
+
+    // Left panel with undo and redo buttons
+    JPanel leftPanel = new JPanel();
+    add(leftPanel, BorderLayout.WEST);
+    leftPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 12, 10));
+    leftPanel.add(btnUndo);
+    leftPanel.add(btnRedo);
+
+    // Right panel with pause button
+    JPanel rightPanel = new JPanel();
+    add(rightPanel, BorderLayout.EAST);
+    rightPanel.setLayout(new FlowLayout(FlowLayout.RIGHT, 12, 10));
+    rightPanel.add(btnPause);
   }
 
 }
