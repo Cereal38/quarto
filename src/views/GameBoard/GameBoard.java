036e2d2a8c54679d31227ef3953b98a3da96c3e7
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-05-05 13:00:25 +0200

    Move pawns to a new component

--- src/views/GameBoard/GameBoard.java
+++ src/views/GameBoard/GameBoard.java
@@ -1,8 +1,6 @@
 package src.views.GameBoard;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class GameBoard extends JPanel {
@@ -13,15 +11,7 @@ public class GameBoard extends JPanel {
     // Add components
     add(new TopBarGameBoard(), BorderLayout.NORTH);
     add(new Board(), BorderLayout.CENTER);
-
-    // Barre avec les pièces disponibles (par exemple, une grille de 4x4)
-    JPanel pieces = new JPanel(new GridLayout(4, 4));
-    for (int i = 0; i < 16; i++) {
-      JButton piece = new JButton(); // Pour représenter les pièces
-      pieces.add(piece);
-    }
-
-    add(pieces, BorderLayout.SOUTH);
+    add(new Pawns(), BorderLayout.SOUTH);
 
   }
 }
