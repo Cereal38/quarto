7161380a1523b1acc533dcd19ee31de896dc90f2
Mohamed Amarcha <simoamarcha@icloud.com>
2024-05-05 14:48:31 +0200

    croissant + read me

--- src/views/components/LanguageButton.java
+++ src/views/components/LanguageButton.java
@@ -8,33 +8,33 @@ import src.views.utils.LangUtils;
 
 public class LanguageButton extends JButton {
 
-  private static final int LANG_EN = 0;
-  private static final int LANG_FR = 1;
-  private int lang = LANG_EN;
+    private static final int LANG_EN = 0;
+    private static final int LANG_FR = 1;
+    private int lang = LANG_EN;
 
-  public LanguageButton() {
+    public LanguageButton() {
 
-    // Load icons
-    ImageIcon frImg = ImageUtils.loadImage("fr.png", 30, 30);
-    ImageIcon enImg = ImageUtils.loadImage("en.png", 30, 30);
+        // Load icons
+        ImageIcon frImg = ImageUtils.loadImage("fr.png", 40, 30);
+        ImageIcon enImg = ImageUtils.loadImage("en.png", 30, 30);
 
-    // Add style
-    setIcon(frImg);
-    setBorder(BorderFactory.createEmptyBorder());
-    setContentAreaFilled(false);
-
-    // Add action
-    setActionCommand("Language");
-    addActionListener(e -> {
-      // Change icon
-      if (lang == LANG_EN) {
-        setIcon(enImg);
-        LangUtils.setLang(LANG_FR);
-      } else if (lang == LANG_FR) {
+        // Add style
         setIcon(frImg);
-        LangUtils.setLang(LANG_EN);
-      }
-      lang = (lang + 1) % 2;
-    });
-  }
+        setBorder(BorderFactory.createEmptyBorder());
+        setContentAreaFilled(false);
+
+        // Add action
+        setActionCommand("Language");
+        addActionListener(e -> {
+            // Change icon
+            if (lang == LANG_EN) {
+                setIcon(enImg);
+                LangUtils.setLang(LANG_FR);
+            } else if (lang == LANG_FR) {
+                setIcon(frImg);
+                LangUtils.setLang(LANG_EN);
+            }
+            lang = (lang + 1) % 2;
+        });
+    }
 }
