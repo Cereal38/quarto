1c4cb48a0084a3b46d72a66d2e5bc657605f1224
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-05-06 22:25:46 +0200

    Display the cells on the board

--- src/views/gameboard/Board.java
+++ src/views/gameboard/Board.java
@@ -1,16 +1,35 @@
 package src.views.gameboard;
 
-import java.awt.GridLayout;
-import javax.swing.JButton;
+import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class Board extends JPanel {
 
-    public Board() {
-        setLayout(new GridLayout(4, 4));
-        for (int i = 0; i < 16; i++) {
-            JButton btn = new JButton(); // Pour représenter les cases du plateau
-            add(btn);
-        }
+  private static final float GAP_FACTOR = (float) 0.3;
+
+  @Override
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+
+    // Get the size of the panel
+    float height = (float) getHeight();
+    float width = (float) getWidth();
+
+    // Compute the cell size and the gap between cells
+    float cellSize = (height - height * GAP_FACTOR) / 4;
+    float gap = (height - cellSize * 4) / 5;
+
+    // Center the board
+    float startX = (float) (width / 2 - cellSize * 2 - gap * 1.5);
+    float startY = (float) gap;
+
+    // Draw the board. A 4x4 grid of outlined circles
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 4; j++) {
+        g.drawOval((int) (startX + i * (cellSize + gap)), (int) (startY + j * (cellSize + gap)), (int) cellSize,
+            (int) cellSize);
+      }
     }
+
+  }
 }
