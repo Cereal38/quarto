1fd339c9270051332901fe0a82160b84114b594b
Adam Jouaid <adam.jouaid123@gmail.com>
2024-05-06 19:24:06 +0200

    renaming and organizing

---
+++ src/views/game/board/GameBoard.java
@@ -0,0 +1,17 @@
+package src.views.gameboard;
+
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+
+public class GameBoard extends JPanel {
+
+    public GameBoard() {
+        setLayout(new BorderLayout());
+
+        // Add components
+        add(new TopBarGameBoard(), BorderLayout.NORTH);
+        add(new Board(), BorderLayout.CENTER);
+        add(new Pawns(), BorderLayout.SOUTH);
+
+    }
+}
