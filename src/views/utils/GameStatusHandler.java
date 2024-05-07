7f71c99bedc206202e50c46d8ff49c6d8840d680
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-05-07 15:09:11 +0200

    Improve GameStatusHandler

--- src/views/utils/GameStatusHandler.java
+++ src/views/utils/GameStatusHandler.java
@@ -2,14 +2,23 @@ package src.views.utils;
 
 public class GameStatusHandler {
 
-  private static int turnOf = -1; // 0: player 1, 1: player 2, -1: null
+  // Constants
+  public static final int GAME_NOT_STARTED = 0;
+  public static final int PLAYER_ONE_PLAY_PAWN = 1;
+  public static final int PLAYER_ONE_SELECT_PAWN = 2;
+  public static final int PLAYER_TWO_PLAY_PAWN = 3;
+  public static final int PLAYER_TWO_SELECT_PAWN = 4;
+  public static final int PLAYER_ONE_WIN = 5;
+  public static final int PLAYER_TWO_WIN = 6;
 
-  public static int getTurnOf() {
-    return turnOf;
+  private static int gamePhase = GAME_NOT_STARTED;
+
+  public static void setGamePhase(int phase) {
+    gamePhase = phase;
   }
 
-  public static void setTurnOf(int turnOf) {
-    GameStatusHandler.turnOf = turnOf;
+  public static int getGamePhase() {
+    return gamePhase;
   }
 
 }
