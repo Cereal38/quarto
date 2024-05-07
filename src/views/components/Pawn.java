b34b984c68e7c0322c7875d9ad7b8ae1ed1f87c0
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-05-07 12:56:50 +0200

    Allow to click and cells and display a fake pawn

--- src/views/components/Pawn.java
+++ src/views/components/Pawn.java
@@ -4,6 +4,7 @@ import java.awt.Cursor;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import src.views.game.board.Board;
 import src.views.game.board.PawnsBar;
 import src.views.utils.ImageUtils;
 
@@ -13,7 +14,7 @@ public class Pawn extends JButton {
 
   private boolean played = false;
 
-  public Pawn(String code, int width, int height, PawnsBar parentBar) {
+  public Pawn(String code, int width, int height, PawnsBar parent) {
     // Load image
     ImageIcon image = ImageUtils.loadImage(code + ".png", width, height);
 
@@ -31,7 +32,31 @@ public class Pawn extends JButton {
           cursorSet = false; // Cursor set permanently to HAND_CURSOR
           setPlayed(true);
           System.err.println("Pawn clicked");
-          parentBar.refresh();
+          parent.refresh();
+        }
+      }
+    });
+  }
+
+  public Pawn(String code, int width, int height, Board parent) {
+    // Load image
+    ImageIcon image = ImageUtils.loadImage(code + ".png", width, height);
+
+    // Set image
+    setIcon(image);
+    setContentAreaFilled(false);
+    setBorder(BorderFactory.createEmptyBorder());
+    setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+    // Set cursor to HAND_CURSOR on the first click
+    addMouseListener(new java.awt.event.MouseAdapter() {
+      public void mouseClicked(java.awt.event.MouseEvent evt) {
+        if (cursorSet) {
+          setCursor(null);
+          cursorSet = false; // Cursor set permanently to HAND_CURSOR
+          setPlayed(true);
+          System.err.println("Pawn clicked");
+          parent.refresh();
         }
       }
     });
--- src/views/game/board/Cell.java
+++ src/views/game/board/Cell.java
@@ -1,6 +1,8 @@
 package src.views.game.board;
 
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.JPanel;
 import src.views.components.Pawn;
 
@@ -8,13 +10,29 @@ public class Cell extends JPanel {
   private int size;
   private Pawn pawn;
 
-  public Cell(int size) {
+  public Cell(int size, Board board) {
     this.size = size;
+
+    addMouseListener(new MouseAdapter() {
+      public void mouseClicked(MouseEvent evt) {
+        System.out.println("Cell clicked");
+        setPawn(new Pawn("1111", 50, 50, board));
+        repaint();
+      }
+    });
+  }
+
+  public void setPawn(Pawn pawn) {
+    this.pawn = pawn;
   }
 
   @Override
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
     g.drawOval(0, 0, size, size);
+    if (pawn != null) {
+      pawn.setBounds(0, 0, size, size);
+      pawn.paint(g);
+    }
   }
 }
