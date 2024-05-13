99212297cd6b45e8549b031b70c1e456135078e7
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-05-13 19:37:40 +0200

    Improve the GameOverDialog

--- src/views/game/board/GameOverDialog.java
+++ src/views/game/board/GameOverDialog.java
@@ -2,30 +2,28 @@ package src.views.game.board;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import src.views.components.TranslatedButton;
+import src.views.components.TranslatedLabel;
 
 public class GameOverDialog extends JPanel {
   private String winnerName;
+  private TranslatedButton btnMenu = new TranslatedButton("main-menu");
+  private TranslatedLabel winnerLabel = new TranslatedLabel("winner-is");
 
   public GameOverDialog(String winnerName) {
     this.winnerName = winnerName;
-    initComponents();
-  }
-
-  private void initComponents() {
-    JLabel winnerLabel = new JLabel("Winner: " + winnerName);
-    JButton newGameButton = new JButton("New game");
-
-    newGameButton.addActionListener(new ActionListener() {
+    btnMenu.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
         System.out.println("New game");
       }
     });
 
+    // TODO: Beautify the dialog
     add(winnerLabel);
-    add(newGameButton);
+    add(new JLabel(winnerName));
+    add(btnMenu);
   }
 }
