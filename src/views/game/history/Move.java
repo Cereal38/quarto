ec6574e6883401a6d8772777fe4e0c6cc7f8f153
Mohamed Amarcha <simoamarcha@icloud.com>
2024-05-16 13:49:53 +0200

    history update

--- src/views/game/history/Move.java
+++ src/views/game/history/Move.java
@@ -1,15 +1,13 @@
 package src.views.game.history;
 
-import java.awt.GridBagLayout;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 
-public class Move extends JPanel {
-    private JLabel moveLbl = new JLabel();
+public class Move extends JLabel {
+    public Move(String text) {
+        super(text);
+    }
 
-    public Move(String move) {
-        setLayout(new GridBagLayout());
-        moveLbl.setText(move);
-        add(moveLbl);
+    public void updateMoveText(String newText) {
+        setText(newText);
     }
 }
