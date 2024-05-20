45eb68212e532be8ea7e1bf8b8998be03d9a4ce2
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-05-20 14:38:13 +0200

    Add margin to the right of the pawns bar to center it

--- src/views/game/board/GameBoard.java
+++ src/views/game/board/GameBoard.java
@@ -9,6 +9,8 @@ import src.views.utils.DimensionUtils;
 
 public class GameBoard extends JPanel {
 
+  private static final int WIDTH_HISTORY = 250;
+
   public GameBoard() {
     setLayout(new BorderLayout());
 
@@ -22,25 +24,26 @@ public class GameBoard extends JPanel {
     topBarGameBoard.setPreferredSize(new Dimension(widthTopBar, heightTopBar));
     DimensionUtils.setBoardTopBar(widthTopBar, heightTopBar);
 
-    // Setup pawns bar
+    // Setup pawns bar. Add a wrapper to add right margin
     PawnsBar pawnsBar = new PawnsBar();
-    int widthPawnsBar = widthFrame;
+    int widthPawnsBar = widthFrame - WIDTH_HISTORY;
     int heightPawnsBar = 70;
     pawnsBar.setPreferredSize(new Dimension(widthPawnsBar, heightPawnsBar));
     DimensionUtils.setBoardPawnsBar(widthPawnsBar, heightPawnsBar);
+    BorderCenterPanel pawnsBarWrapper = new BorderCenterPanel(pawnsBar, 0, 0, 0, WIDTH_HISTORY);
 
     BoardWrapper boardWrapper = new BoardWrapper();
 
     MovesHistory movesHistory = new MovesHistory();
     BorderCenterPanel movesHistoryWrapper = new BorderCenterPanel(movesHistory, 50, 0, 50, 0);
-    movesHistoryWrapper.setPreferredSize(new Dimension(250, heightFrame - heightTopBar - heightPawnsBar));
-    DimensionUtils.setHistory(250, heightFrame - heightTopBar - heightPawnsBar);
+    movesHistoryWrapper.setPreferredSize(new Dimension(WIDTH_HISTORY, heightFrame - heightTopBar - heightPawnsBar));
+    DimensionUtils.setHistory(WIDTH_HISTORY, heightFrame - heightTopBar - heightPawnsBar);
 
     // Add components
     add(topBarGameBoard, BorderLayout.NORTH);
     add(boardWrapper, BorderLayout.CENTER);
     add(movesHistoryWrapper, BorderLayout.EAST);
-    add(pawnsBar, BorderLayout.SOUTH);
+    add(pawnsBarWrapper, BorderLayout.SOUTH);
 
   }
 }
