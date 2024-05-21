58cbc42f825f30616544cd892dd640195d268df3
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-05-21 11:06:55 +0200

    Improve the responsivness of the pawns bar

--- src/views/game/board/GameBoard.java
+++ src/views/game/board/GameBoard.java
@@ -15,8 +15,8 @@ public class GameBoard extends JPanel implements GameStatusListener {
 
   private static final int WIDTH_HISTORY = 250;
   private static final int HEIGHT_TOP_BAR = 50;
-  private static final int HEIGHT_PAWNS_BAR = 70;
   private static final float BOARD_GAP_FACTOR = (float) 0.3;
+  private static final int PAWNS_BAR_GAP = 10;
 
   public GameBoard() {
 
@@ -53,9 +53,10 @@ public class GameBoard extends JPanel implements GameStatusListener {
     int heightFrame = DimensionUtils.getMainFrameHeight();
     int widthTopBar = widthFrame;
     int widthPawnsBar = widthFrame - WIDTH_HISTORY;
-    int pawnsBarPawnSize = widthPawnsBar / 16;
+    int pawnsBarPawnSize = Math.min((widthPawnsBar / 8) - (PAWNS_BAR_GAP * 2), 80);
+    int heightPawnsBar = pawnsBarPawnSize * 2 + PAWNS_BAR_GAP * 2;
     int widthBoardWrapper = widthFrame - WIDTH_HISTORY;
-    int heightBoardWrapper = heightFrame - HEIGHT_TOP_BAR - HEIGHT_PAWNS_BAR;
+    int heightBoardWrapper = heightFrame - HEIGHT_TOP_BAR - heightPawnsBar;
     int boardCellSize = (int) (heightBoardWrapper - heightBoardWrapper * BOARD_GAP_FACTOR) / 4;
     int boardGap = (heightBoardWrapper - boardCellSize * 4) / 5;
     int horizontalMarginBoard = (int) (widthBoardWrapper / 2 - boardCellSize * 2 - boardGap * 1.5);
@@ -69,8 +70,8 @@ public class GameBoard extends JPanel implements GameStatusListener {
     TopBarGameBoard topBarGameBoard = new TopBarGameBoard(widthTopBar, HEIGHT_TOP_BAR);
 
     // Setup pawns bar. Add a wrapper to add right margin
-    DimensionUtils.setBoardPawnsBar(widthPawnsBar, HEIGHT_PAWNS_BAR);
-    PawnsBar pawnsBar = new PawnsBar(widthPawnsBar, HEIGHT_PAWNS_BAR);
+    DimensionUtils.setBoardPawnsBar(widthPawnsBar, heightPawnsBar);
+    PawnsBar pawnsBar = new PawnsBar(widthPawnsBar, heightPawnsBar, PAWNS_BAR_GAP);
     BorderCenterPanel pawnsBarWrapper = new BorderCenterPanel(pawnsBar, 0, 0, 0, WIDTH_HISTORY);
 
     // Setup board. Add a wrapper to center the board and add margins
@@ -81,8 +82,8 @@ public class GameBoard extends JPanel implements GameStatusListener {
     // Setup history
     MovesHistory movesHistory = new MovesHistory();
     BorderCenterPanel movesHistoryWrapper = new BorderCenterPanel(movesHistory, 50, 0, 50, 0);
-    movesHistoryWrapper.setPreferredSize(new Dimension(WIDTH_HISTORY, heightFrame - HEIGHT_TOP_BAR - HEIGHT_PAWNS_BAR));
-    DimensionUtils.setHistory(WIDTH_HISTORY, heightFrame - HEIGHT_TOP_BAR - HEIGHT_PAWNS_BAR);
+    movesHistoryWrapper.setPreferredSize(new Dimension(WIDTH_HISTORY, heightFrame - HEIGHT_TOP_BAR - heightPawnsBar));
+    DimensionUtils.setHistory(WIDTH_HISTORY, heightFrame - HEIGHT_TOP_BAR - heightPawnsBar);
 
     // Add components
     add(topBarGameBoard, BorderLayout.NORTH);
