1adad1f796c2c99dc8f28063e12925be8215a15e
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-05-22 17:52:39 +0200

    Change the board layout and add pawns bar background

--- src/views/game/board/GameBoard.java
+++ src/views/game/board/GameBoard.java
@@ -1,19 +1,17 @@
 package src.views.game.board;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import javax.swing.JPanel;
 import src.views.components.BorderCenterPanel;
-import src.views.game.history.MovesHistory;
 import src.views.listeners.GameStatusListener;
 import src.views.utils.DimensionUtils;
 import src.views.utils.GameStatusHandler;
 
 public class GameBoard extends JPanel implements GameStatusListener {
 
-  private static final int WIDTH_HISTORY = 250;
+  private static final int WIDTH_PAWNS_BAR = 250;
   private static final int HEIGHT_TOP_BAR = 60;
   private static final float BOARD_GAP_FACTOR = (float) 0.3;
   private static final int PAWNS_BAR_GAP = 10;
@@ -52,11 +50,11 @@ public class GameBoard extends JPanel implements GameStatusListener {
     int widthFrame = DimensionUtils.getMainFrameWidth();
     int heightFrame = DimensionUtils.getMainFrameHeight();
     int widthTopBar = widthFrame;
-    int widthPawnsBar = widthFrame - WIDTH_HISTORY;
-    int pawnsBarPawnSize = Math.min((widthPawnsBar / 8) - (PAWNS_BAR_GAP * 2), 80);
-    int heightPawnsBar = pawnsBarPawnSize * 2 + PAWNS_BAR_GAP * 2;
-    int widthBoardWrapper = widthFrame - WIDTH_HISTORY;
-    int heightBoardWrapper = heightFrame - HEIGHT_TOP_BAR - heightPawnsBar;
+    int widthPawnsBar = WIDTH_PAWNS_BAR;
+    int pawnsBarPawnSize = (WIDTH_PAWNS_BAR / 2) - 10;
+    int heightPawnsBar = heightFrame - HEIGHT_TOP_BAR;
+    int widthBoardWrapper = widthFrame - WIDTH_PAWNS_BAR;
+    int heightBoardWrapper = heightFrame - HEIGHT_TOP_BAR;
     int boardCellSize = (int) (heightBoardWrapper - heightBoardWrapper * BOARD_GAP_FACTOR) / 4;
     int boardGap = (heightBoardWrapper - boardCellSize * 4) / 5;
     int horizontalMarginBoard = (int) (widthBoardWrapper / 2 - boardCellSize * 2 - boardGap * 1.5);
@@ -69,10 +67,9 @@ public class GameBoard extends JPanel implements GameStatusListener {
     // Setup top bar
     TopBarGameBoard topBarGameBoard = new TopBarGameBoard(widthTopBar, HEIGHT_TOP_BAR);
 
-    // Setup pawns bar. Add a wrapper to add right margin
+    // Setup pawns bar
     DimensionUtils.setBoardPawnsBar(widthPawnsBar, heightPawnsBar);
     PawnsBar pawnsBar = new PawnsBar(widthPawnsBar, heightPawnsBar, PAWNS_BAR_GAP);
-    BorderCenterPanel pawnsBarWrapper = new BorderCenterPanel(pawnsBar, 0, 0, 0, WIDTH_HISTORY);
 
     // Setup board. Add a wrapper to center the board and add margins
     BorderCenterPanel boardWrapper = new BorderCenterPanel(
@@ -80,16 +77,18 @@ public class GameBoard extends JPanel implements GameStatusListener {
         verticalMarginBoard, horizontalMarginBoard);
 
     // Setup history
-    MovesHistory movesHistory = new MovesHistory();
-    BorderCenterPanel movesHistoryWrapper = new BorderCenterPanel(movesHistory, 50, 0, 50, 0);
-    movesHistoryWrapper.setPreferredSize(new Dimension(WIDTH_HISTORY, heightFrame - HEIGHT_TOP_BAR - heightPawnsBar));
-    DimensionUtils.setHistory(WIDTH_HISTORY, heightFrame - HEIGHT_TOP_BAR - heightPawnsBar);
+    // MovesHistory movesHistory = new MovesHistory();
+    // BorderCenterPanel movesHistoryWrapper = new BorderCenterPanel(movesHistory,
+    // 50, 0, 50, 0);
+    // movesHistoryWrapper.setPreferredSize(new Dimension(WIDTH_HISTORY, heightFrame
+    // - HEIGHT_TOP_BAR - heightPawnsBar));
+    // DimensionUtils.setHistory(WIDTH_HISTORY, heightFrame - HEIGHT_TOP_BAR -
+    // heightPawnsBar);
 
     // Add components
     add(topBarGameBoard, BorderLayout.NORTH);
     add(boardWrapper, BorderLayout.CENTER);
-    add(movesHistoryWrapper, BorderLayout.EAST);
-    add(pawnsBarWrapper, BorderLayout.SOUTH);
+    add(pawnsBar, BorderLayout.EAST);
 
     revalidate();
     repaint();
--- src/views/game/board/PawnsBar.java
+++ src/views/game/board/PawnsBar.java
@@ -2,12 +2,17 @@ package src.views.game.board;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Image;
+import java.io.File;
 import java.util.List;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import src.views.components.Pawn;
 import src.views.utils.EventsHandler;
 
 public class PawnsBar extends JPanel {
+
+  private Image bgImage;
 
   public PawnsBar(int width, int height, int gap) {
     setLayout(new FlowLayout(FlowLayout.CENTER, gap, gap));
@@ -21,6 +26,20 @@ public class PawnsBar extends JPanel {
       add(pawn);
     }
 
+    try {
+      // TODO: Load bg only once
+      bgImage = ImageIO.read(new File("assets/images/pawns-bar.png"));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  @Override
+  protected void paintComponent(java.awt.Graphics g) {
+    super.paintComponent(g);
+    // Draw background image
+    g.drawImage(bgImage, 0, 0, getWidth(), getHeight(), this);
   }
 
 }
