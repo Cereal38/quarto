478955c8b77e85aaa5379935a2c8cee96995ea3f
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-05-22 08:27:58 +0200

    Add the right panel

---
+++ src/views/game/board/RightPanelTopBar.java
@@ -0,0 +1,43 @@
+package src.views.game.board;
+
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.io.File;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import src.views.utils.EventsHandler;
+
+public class RightPanelTopBar extends JPanel {
+
+  private Image bgImage;
+
+  public RightPanelTopBar() {
+    setLayout(new FlowLayout(FlowLayout.LEFT, 12, 10));
+
+    PauseMenuButton btnPause = new PauseMenuButton();
+
+    EventsHandler.setPauseMenuButton(btnPause);
+
+    // Add action listeners to the buttons
+    btnPause.addActionListener(e -> {
+      EventsHandler.showDialog(new PauseDialogContent(), true);
+    });
+
+    add(btnPause);
+
+    // Load background image
+    try {
+      bgImage = ImageIO.read(new File("assets/images/gameboard-action-buttons-container.png"));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  protected void paintComponent(java.awt.Graphics g) {
+    super.paintComponent(g);
+    // Draw background image
+    g.drawImage(bgImage, 0, 0, getWidth(), getHeight(), this);
+  }
+
+}
--- src/views/game/board/TopBarGameBoard.java
+++ src/views/game/board/TopBarGameBoard.java
@@ -2,9 +2,7 @@ package src.views.game.board;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import javax.swing.JPanel;
-import src.views.utils.EventsHandler;
 
 public class TopBarGameBoard extends JPanel {
 
@@ -13,25 +11,13 @@ public class TopBarGameBoard extends JPanel {
     setLayout(new BorderLayout());
     setPreferredSize(new Dimension(width, height));
 
-    // Create elements
-    PauseMenuButton btnPause = new PauseMenuButton();
-
-    EventsHandler.setPauseMenuButton(btnPause);
-
-    // Add action listeners to the buttons
-    btnPause.addActionListener(e -> {
-      EventsHandler.showDialog(new PauseDialogContent(), true);
-    });
-
     // Left panel with undo and redo buttons
     JPanel leftPanel = new LeftPanelTopBar();
     add(leftPanel, BorderLayout.WEST);
 
     // Right panel with pause button
-    JPanel rightPanel = new JPanel();
+    JPanel rightPanel = new RightPanelTopBar();
     add(rightPanel, BorderLayout.EAST);
-    rightPanel.setLayout(new FlowLayout(FlowLayout.RIGHT, 12, 10));
-    rightPanel.add(btnPause);
 
     // Center panel with the current state of the game
     JPanel centerPanel = new CenterPanelTopBar();
