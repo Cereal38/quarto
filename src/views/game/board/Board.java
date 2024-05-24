23b554cbfd6777f0613a3f234c0de1837a948acf
Mohamed Amarcha <simoamarcha@icloud.com>
2024-05-24 15:47:34 +0200

    herbe background

--- src/views/game/board/Board.java
+++ src/views/game/board/Board.java
@@ -6,19 +6,21 @@ import java.awt.Image;
 import java.io.File;
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javafx.scene.paint.Color;
 import src.views.utils.EventsHandler;
 
 public class Board extends JPanel {
 
   private Cell[][] cells = new Cell[4][4];
   private Image bgImage;
+  private Image backGroundImage;
 
   public Board(int width, int height, int cellSize) {
 
     setLayout(new GridLayout(4, 4));
     setPreferredSize(new Dimension(width, height));
-
     // Create a 20-pixel margin around the grid
     // TODO: Change that
     setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
@@ -36,6 +38,7 @@ public class Board extends JPanel {
     try {
       // TODO: Load board only once
       bgImage = ImageIO.read(new File("assets/images/board.png"));
+    backGroundImage = new ImageIcon(getClass().getResource("/assets/images/bg-board.png")).getImage();
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -45,6 +48,7 @@ public class Board extends JPanel {
   protected void paintComponent(java.awt.Graphics g) {
     super.paintComponent(g);
     // Draw background image
+    g.drawImage(backGroundImage, 0, 0, getWidth(), getHeight(), this);
     g.drawImage(bgImage, 0, 0, getWidth(), getHeight(), this);
   }
 
