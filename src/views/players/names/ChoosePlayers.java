bd3ff2fb680ddb6b192c084f8b24af0e770d78aa
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-05-24 09:27:10 +0200

    Improve players name layout

--- src/views/players/names/ChoosePlayers.java
+++ src/views/players/names/ChoosePlayers.java
@@ -1,11 +1,12 @@
 package src.views.players.names;
 
-import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import src.views.components.TranslatedLabel;
 import src.views.utils.DimensionUtils;
 
 public class ChoosePlayers extends JPanel {
@@ -31,18 +32,15 @@ public class ChoosePlayers extends JPanel {
     // Clear the board
     removeAll();
 
-    setLayout(new BorderLayout());
+    setLayout(new GridLayout(3, 1));
 
     // Compute dimensions
-    int widthFrame = DimensionUtils.getMainFrameWidth();
     int heightFrame = DimensionUtils.getMainFrameHeight();
     int heightPawnsBar = heightFrame - HEIGHT_TOP_BAR;
     int widthPawnsBar = (int) (heightPawnsBar / 3.81); // We want to keep the ratio 3.81 (height/width)
     int pawnsBarPawnSize = Math.min((widthPawnsBar / 2) - 30, (heightPawnsBar / 8) - 30);
     int heightBoardWrapper = heightFrame - HEIGHT_TOP_BAR;
     int boardCellSize = (int) (heightBoardWrapper - heightBoardWrapper * 0.5) / 4;
-    int horizontalMarginFields = widthFrame / 4;
-    int verticalMarginFields = heightFrame / 4;
 
     // Register useful dimensions
     DimensionUtils.setBarCellSize(pawnsBarPawnSize);
@@ -50,9 +48,11 @@ public class ChoosePlayers extends JPanel {
     DimensionUtils.setBoardPawnsBar(widthPawnsBar, heightPawnsBar);
 
     // Setup title bar
-    JPanel titleBar = new JPanel();
-    JLabel titleLabel = new JLabel("Choose players");
-    titleBar.add(titleLabel);
+    JPanel titleWrapper = new JPanel();
+    titleWrapper.setLayout(new GridBagLayout());
+    TranslatedLabel titleLabel = new TranslatedLabel("enter-players-names");
+    titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+    titleWrapper.add(titleLabel);
 
     // Setup fields
     JPanel fieldsWrapper = new JPanel();
@@ -60,8 +60,8 @@ public class ChoosePlayers extends JPanel {
     fieldsWrapper.add(new PlayerFields());
 
     // Add components
-    add(titleBar, BorderLayout.NORTH);
-    add(fieldsWrapper, BorderLayout.CENTER);
+    add(titleWrapper);
+    add(fieldsWrapper);
 
     revalidate();
     repaint();
--- src/views/players/names/PlayerFields.java
+++ src/views/players/names/PlayerFields.java
@@ -3,6 +3,8 @@ package src.views.players.names;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -17,32 +19,55 @@ public class PlayerFields extends JPanel {
   private CustomizedButton btnSwitchPlayer1 = new CustomizedButton("switch-to-ai");
   private JTextField namePlayer2 = new JTextField();
   private CustomizedButton btnSwitchPlayer2 = new CustomizedButton("switch-to-ai");
+  private CustomizedButton btnStartGame = new CustomizedButton("start");
 
   public PlayerFields() {
-    setLayout(new FlowLayout(FlowLayout.CENTER, 50, 0));
+
+    setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+
+    int componentHeight = 45;
+    int componentWidth = 200;
+    int spacing = 10;
+    int vsLabelWidth = 100;
+    int startButtonWidth = 2 * componentWidth + vsLabelWidth;
+
+    JPanel playerFieldsWrapper = new JPanel();
+    playerFieldsWrapper.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
 
     // First player
     JPanel player1Panel = new JPanel();
-    player1Panel.setLayout(new GridLayout(2, 1, 0, 10));
-    player1Panel.setPreferredSize(new Dimension(200, 100));
+    player1Panel.setLayout(new GridLayout(2, 1, 0, spacing));
+    player1Panel.setPreferredSize(new Dimension(componentWidth, componentHeight * 2 + spacing));
     player1Panel.add(namePlayer1);
     player1Panel.add(btnSwitchPlayer1);
 
     // VS
     JPanel vsPanel = new JPanel();
+    vsPanel.setPreferredSize(new Dimension(vsLabelWidth, componentHeight));
     vsPanel.add(new JLabel("VS"));
 
     // Second player
     JPanel player2Panel = new JPanel();
-    player2Panel.setLayout(new GridLayout(2, 1, 0, 10));
-    player2Panel.setPreferredSize(new Dimension(200, 100));
+    player2Panel.setLayout(new GridLayout(2, 1, 0, spacing));
+    player2Panel.setPreferredSize(new Dimension(componentWidth, componentHeight * 2 + spacing));
     player2Panel.add(namePlayer2);
     player2Panel.add(btnSwitchPlayer2);
 
-    add(player1Panel);
-    add(vsPanel);
-    add(player2Panel);
+    playerFieldsWrapper.add(player1Panel);
+    playerFieldsWrapper.add(vsPanel);
+    playerFieldsWrapper.add(player2Panel);
 
+    // Start button
+    JPanel startButtonWrapper = new JPanel();
+    startButtonWrapper.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+    JPanel startButtonPanel = new JPanel();
+    startButtonPanel.setLayout(new GridLayout(1, 1));
+    startButtonPanel.setPreferredSize(new Dimension(startButtonWidth, componentHeight));
+    startButtonWrapper.add(startButtonPanel);
+    startButtonPanel.add(btnStartGame);
+
+    add(playerFieldsWrapper);
+    add(Box.createRigidArea(new Dimension(0, spacing))); // Divider
+    add(startButtonWrapper);
   }
-
 }
