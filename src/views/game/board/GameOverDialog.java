2a559d68ba6c281b5197c05fc400e2e70a01cb9f
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-05-28 11:41:26 +0200

    Init back button

--- src/views/game/board/GameOverDialog.java
+++ src/views/game/board/GameOverDialog.java
@@ -4,13 +4,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import src.views.components.TranslatedButton;
+import src.views.components.CustomizedButton;
 import src.views.components.TranslatedLabel;
 import src.views.utils.EventsHandler;
 
 public class GameOverDialog extends JPanel {
   private String winnerName;
-  private TranslatedButton btnMenu = new TranslatedButton("main-menu");
+  private CustomizedButton btnBack = new CustomizedButton("back-to-game");
+  private CustomizedButton btnMenu = new CustomizedButton("main-menu");
   private TranslatedLabel winnerLabel = new TranslatedLabel("winner-is");
 
   public GameOverDialog(String winnerName) {
@@ -23,9 +24,17 @@ public class GameOverDialog extends JPanel {
       }
     });
 
+    btnBack.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        EventsHandler.hideDialog();
+      }
+    });
+
     // TODO: Beautify the dialog
     add(winnerLabel);
     add(new JLabel(winnerName));
+    add(btnBack);
     add(btnMenu);
   }
 }
