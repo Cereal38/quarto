9e672b14da52e3c8c26358f80b7ffb58b28462a0
Adam Jouaid <adam.jouaid123@gmail.com>
2024-06-02 20:51:35 +0200

    styling top bar main menu

--- src/views/main/menu/TopBarMainMenu.java
+++ src/views/main/menu/TopBarMainMenu.java
@@ -2,6 +2,8 @@ package src.views.main.menu;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Image;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import src.views.components.ExitButton;
@@ -10,12 +12,15 @@ import src.views.components.ManualButton;
 import src.views.components.MusicButton;
 import src.views.components.ThemeButton;
 import src.views.utils.EventsHandler;
+import src.views.utils.ImageUtils;
 
 public class TopBarMainMenu extends JPanel {
 
   private JButton musicButton, langButton, modeButton, exitButton, bookButton;
+  private Image woodTexture;
 
   public TopBarMainMenu() {
+    woodTexture = ImageUtils.loadImage("wood.jpeg", 50, 50).getImage();
 
     // Create buttons
     musicButton = new MusicButton();
@@ -24,22 +29,22 @@ public class TopBarMainMenu extends JPanel {
     exitButton = new ExitButton();
     bookButton = new ManualButton();
 
-    // set a listener to the buttons
+    // Set listeners to the buttons
     EventsHandler.setMusicButton((MusicButton) musicButton);
     EventsHandler.setLanguageButton((LanguageButton) langButton);
     EventsHandler.setManualButton((ManualButton) bookButton);
     EventsHandler.setExitButton((ExitButton) exitButton);
 
     // Buttons aligned on the left
-    JPanel leftPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 12, 10));
-    leftPanel.setOpaque(false);
+    JPanel leftPanel = new LeftPanel();
+    leftPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 12, 10));
     leftPanel.add(musicButton);
     leftPanel.add(modeButton);
     leftPanel.add(langButton);
 
     // Buttons aligned on the right
-    JPanel rightPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 12, 10));
-    rightPanel.setOpaque(false);
+    JPanel rightPanel = new RightPanel();
+    rightPanel.setLayout(new FlowLayout(FlowLayout.RIGHT, 12, 10));
     rightPanel.add(bookButton);
     rightPanel.add(exitButton);
 
@@ -50,6 +55,11 @@ public class TopBarMainMenu extends JPanel {
 
     // Transparent background
     setOpaque(false);
+  }
 
+  @Override
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    g.drawImage(woodTexture, 0, 0, this.getWidth(), this.getHeight(), this);
   }
 }
