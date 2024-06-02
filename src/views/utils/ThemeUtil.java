c1fb349de908bbf9dee1f9763388cda85430220d
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-06-02 17:27:36 +0200

    Create the themeUtil

---
+++ src/views/utils/ThemeUtil.java
@@ -0,0 +1,11 @@
+package src.views.utils;
+
+/**
+ * This class allow to change the theme of the application.
+ */
+public class ThemeUtil {
+
+  private static final int LIGHT = 0;
+  private static final int DARK = 1;
+
+}
