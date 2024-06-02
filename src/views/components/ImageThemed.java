67723be1f08384e2b089953a15d1bfca1bf66ad2
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-06-02 19:03:55 +0200

    Simplify ImageThemed code

--- src/views/components/ImageThemed.java
+++ src/views/components/ImageThemed.java
@@ -1,19 +1,14 @@
 package src.views.components;
 
 import java.awt.Image;
-import java.util.ArrayList;
-import java.util.List;
-import javax.swing.JComponent;
-import src.views.listeners.ThemeListener;
 import src.views.utils.ImageLibrary;
 import src.views.utils.ThemeUtils;
 
-public class ImageThemed implements ThemeListener {
+public class ImageThemed {
 
   private String name;
   private Image imageLight;
   private Image imageDark;
-  private static final List<ThemeListener> listeners = new ArrayList<>();
 
   public ImageThemed(String name) {
     this.name = name;
@@ -22,19 +17,6 @@ public class ImageThemed implements ThemeListener {
     this.imageLight = ImageLibrary.getImage("light", name);
     this.imageDark = ImageLibrary.getImage("dark", name);
 
-    ThemeUtils.addThemeListener(this);
-    listeners.add(this);
-    updatedTheme();
-  }
-
-  @Override
-  public void updatedTheme() {
-    // Call repaint() on all registered ThemeListener components
-    for (ThemeListener listener : listeners) {
-      if (listener instanceof JComponent) {
-        ((JComponent) listener).repaint();
-      }
-    }
   }
 
   public Image getImage() {
