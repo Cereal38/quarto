42f43933d38da7ea1e30cd7e253639a30d6a8dcf
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-06-02 17:31:43 +0200

    Implement listeners in ThemeUtil

--- src/views/utils/ThemeUtil.java
+++ src/views/utils/ThemeUtil.java
@@ -1,5 +1,9 @@
 package src.views.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+import src.views.listeners.ThemeListener;
+
 /**
  * This class allow to change the theme of the application.
  */
@@ -7,5 +11,30 @@ public class ThemeUtil {
 
   private static final int LIGHT = 0;
   private static final int DARK = 1;
+  private static int theme = LIGHT;
+
+  /**
+   * The list of theme listeners.
+   */
+  private static final List<ThemeListener> listeners = new ArrayList<>();
+
+  public static void addLanguageChangeListener(ThemeListener listener) {
+    listeners.add(listener);
+  }
+
+  public static void removeLanguageChangeListener(ThemeListener listener) {
+    listeners.remove(listener);
+  }
+
+  public static void setTheme(int theme) {
+    ThemeUtil.theme = theme;
+    for (ThemeListener listener : listeners) {
+      listener.updatedTheme();
+    }
+  }
+
+  public static int getTheme() {
+    return theme;
+  }
 
 }
