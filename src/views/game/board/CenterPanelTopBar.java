c542c7c28bf3fea319e2b162a52af1187e9c24d8
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2024-06-02 22:19:36 +0200

    Top bar game board

--- src/views/game/board/CenterPanelTopBar.java
+++ src/views/game/board/CenterPanelTopBar.java
@@ -1,23 +1,25 @@
 package src.views.game.board;
 
 import java.awt.GridLayout;
-import java.awt.Image;
-import java.io.File;
-import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import src.views.components.Field;
+import src.views.components.ImageThemed;
 import src.views.components.TranslatedString;
+import src.views.listeners.ThemeListener;
 import src.views.utils.EventsHandler;
+import src.views.utils.ThemeUtils;
 
-public class CenterPanelTopBar extends JPanel {
+public class CenterPanelTopBar extends JPanel implements ThemeListener {
 
-  private Image bgImage;
+  private ImageThemed bgImage = new ImageThemed("gameboard-center-top-bar.png");
   private Field player1Name;
   private Field player2Name;
   private Field selectionPhase;
   private Field playPhase;
 
   public CenterPanelTopBar() {
+    ThemeUtils.addThemeListener(this);
+
     setLayout(new GridLayout(1, 4));
 
     String selectionPhaseStr = new TranslatedString("select-pawn").getText();
@@ -34,19 +36,18 @@ public class CenterPanelTopBar extends JPanel {
     add(selectionPhase);
     add(playPhase);
 
-    // Load background image
-    try {
-      bgImage = ImageIO.read(new File("assets/images/gameboard-center-top-bar.png"));
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
   }
 
   @Override
   protected void paintComponent(java.awt.Graphics g) {
     super.paintComponent(g);
     // Draw background image
-    g.drawImage(bgImage, 0, 0, getWidth(), getHeight(), this);
+    g.drawImage(bgImage.getImage(), 0, 0, getWidth(), getHeight(), this);
+  }
+
+  @Override
+  public void updatedTheme() {
+    repaint();
   }
 
 }
