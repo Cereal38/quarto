26d7e1b69d64642b6d95fbc94b944343a8b9afac
Adam Jouaid <adam.jouaid123@gmail.com>
2024-06-03 18:15:00 +0200

    added doc for translatedlabel

--- src/views/components/TranslatedLabel.java
+++ src/views/components/TranslatedLabel.java
@@ -2,16 +2,29 @@ package src.views.components;
 
 import javax.swing.JLabel;
 
+/**
+ * A label with translated text.
+ */
 public class TranslatedLabel extends JLabel {
 
   TranslatedString translatedString;
 
+  /**
+   * Constructs a new TranslatedLabel with the specified translation key.
+   *
+   * @param key the translation key for the label text
+   */
   public TranslatedLabel(String key) {
     super();
     translatedString = new TranslatedString(key, this);
     setText(translatedString.getText());
   }
 
+  /**
+   * Sets the translation key for the label text and updates the text accordingly.
+   *
+   * @param key the translation key for the label text
+   */
   public void setKey(String key) {
     translatedString.setKey(key);
     setText(translatedString.getText());
