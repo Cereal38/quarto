bc17284336c94335e5abe9f36e7d53c1f01a2c43
Adam Jouaid <adam.jouaid123@gmail.com>
2024-06-03 18:24:03 +0200

    added doc for pauseBut

--- src/views/game/board/PauseButton.java
+++ src/views/game/board/PauseButton.java
@@ -8,12 +8,17 @@ import src.views.components.TranslatedString;
 import src.views.listeners.ThemeListener;
 import src.views.utils.GameStatusHandler;
 
+/**
+ * Represents a button used to pause the game.
+ */
 public class PauseButton extends JButton implements ThemeListener {
   private TranslatedString tooltip;
   private ImageThemed image = new ImageThemed("pause.png");
 
+  /**
+   * Constructs a PauseButton object.
+   */
   public PauseButton() {
-
     setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
 
     // Add style
@@ -30,6 +35,9 @@ public class PauseButton extends JButton implements ThemeListener {
     tooltip = new TranslatedString("pause-tooltip", this, true);
   }
 
+  /**
+   * Updates the theme of the button.
+   */
   @Override
   public void updatedTheme() {
     ImageIcon icon = new ImageIcon(image.getImage());
